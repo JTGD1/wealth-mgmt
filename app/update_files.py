f1f7a84e45a9c4121e4125b650289a30ee09a494
Justin Davda <justindavda@gmail.com>
2020-06-27 19:18:59 -0400

    update files completed for all processes needed

--- app/update_files.py
+++ app/update_files.py
@@ -40,7 +40,7 @@ def update_etp(asset_class,identifier,units,purchase_price):
     isThere = False 
     etp_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "exchange_traded_products.csv")
     df_etp = pd.read_csv(etp_filepath)
-    
+    #check inputs
     try:
         check_asset_class = str(asset_class)
         check_identifier = str(identifier)
@@ -49,14 +49,12 @@ def update_etp(asset_class,identifier,units,purchase_price):
         
         if 0 > check_units or check_purchase_price < 0:
             invalid = "Invalid number - please check and try again"
-            #print(invalid)
             return invalid            
 
     except ValueError:
         error = "Invalid format - please check and try again"
-        #print(error)
         return error        
-
+    #update or append
     for ind in df_etp.index:   
         if df_etp["Identifier"][ind] == identifier:              
 
@@ -83,7 +81,7 @@ def update_etp(asset_class,identifier,units,purchase_price):
             df_etp.to_csv(etp_filepath, index = False) 
             action = "updated"
         
-    #print(ticker,action)
+    #print(identifier,action)
     return action   
 
 def delete_etp(identifier):
@@ -93,5 +91,121 @@ def delete_etp(identifier):
     
     delete_item(identifier, df, filepath)
     return
+
+
+def delete_asset(identifier):
+
+    filepath = os.path.join(os.path.dirname(__file__),
+                            "..", "data", "other_assets.csv")
+    df = pd.read_csv(filepath)
+
+    delete_item(identifier, df, filepath)
+    return
     
+
+def delete_liability(identifier):
+
+    filepath = os.path.join(os.path.dirname(__file__),
+                            "..", "data", "liabilities.csv")
+    df = pd.read_csv(filepath)
+
+    delete_item(identifier, df, filepath)
+    return
+
+def update_asset(asset_class,identifier,current_value):
+    #pull in data from csv
+    isThere = False 
+    asset_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "other_assets.csv")
+    df_asset = pd.read_csv(asset_filepath)
+    #check inputs
+    try:
+        check_asset_class = str(asset_class)
+        check_identifier = str(identifier)
+        check_current_value = float(current_value)
+        
+        if check_current_value < 0:
+            invalid = "Invalid number - please check and try again"
+            return invalid            
+
+    except ValueError:
+        error = "Invalid format - please check and try again"
+        return error        
+    #update or append
+    for ind in df_asset.index:   
+        if df_asset["Identifier"][ind] == identifier:              
+
+        #if ticker already exists, just update current value
+        
+            df_asset.loc[df_asset["Identifier"] == identifier, ["Current Value"]] = round(current_value,2)
+            isThere = True
+    
+        #if ticker doesn't exist, add it to the end and make purchase price = current value
+
+        if isThere != True:
+            df2 = pd.DataFrame({"Asset Class":[asset_class], 
+            "Identifier":[identifier], 
+            "Purchase Value":[current_value],"Current Value":[current_value]}) 
+
+            dff = df_asset.append(df2, ignore_index = True)
+            dff.to_csv(asset_filepath, index = False)
+
+            action = "added"            
+            
+        else:
+            
+            df_asset.to_csv(asset_filepath, index = False) 
+            action = "updated"
+        
+    print(identifier, action)
+    return action
+
+
+def update_liability(asset_class, identifier, current_value):
+    #pull in data from csv
+    isThere = False
+    liability_filepath = os.path.join(os.path.dirname(
+        __file__), "..", "data", "liabilities.csv")
+    df_liability = pd.read_csv(liability_filepath)
+    #check inputs
+    try:
+        check_asset_class = str(asset_class)
+        check_identifier = str(identifier)
+        check_current_value = float(current_value)
+
+        if check_current_value < 0:
+            invalid = "Invalid number - please check and try again"
+            return invalid
+
+    except ValueError:
+        error = "Invalid format - please check and try again"
+        return error
+    #update or append
+    for ind in df_liability.index:
+        if df_liability["Identifier"][ind] == identifier:
+
+            #if ticker already exists, just update current value
+
+            df_liability.loc[df_asset["Identifier"] == identifier,
+                         ["Current Value"]] = round(current_value, 2)
+            isThere = True
+
+        #if ticker doesn't exist, add it to the end and make purchase price = current value
+
+        if isThere != True:
+            df2 = pd.DataFrame({"Asset Class": [asset_class],
+                                "Identifier": [identifier],
+                                "Purchase Value": [current_value], "Current Value": [current_value]})
+
+            dff = df_liability.append(df2, ignore_index=True)
+            dff.to_csv(liability_filepath, index=False)
+
+            action = "added"
+
+        else:
+
+            df_liability.to_csv(asset_filepath, index=False)
+            action = "updated"
+
+    print(identifier, action)
+    return action
 
--- app/user_interface.py
+++ app/user_interface.py
@@ -1,16 +1,121 @@
 # user_interface.py
 # basic menus and decision-making around what user wants to do
 
+import tkinter
 from app.update_files import update_etp
+from app.update_files import update_asset
+from app.update_files import update_liability
 from app.update_files import delete_etp
-
+from app.update_files import delete_liability
+from app.update_files import delete_asset
 import os
+import tkinter
 
-asset_class = "Equity"
-identifier = "GOOG"
-units = 10
-purchase_price = 100
+#asset_class = "Equity"
+#identifier = "GOOG"
+#units = 10
+#purchase_price = 100#
 
-#update_ETP(asset_class, identifier, units, purchase_price)
+##update_ETP(asset_class, identifier, units, purchase_price)#
 
-delete_etp(identifier)
+#delete_etp(identifier)
+
+asset_class = "Real Estate"
+identifier = "Hamptons"
+purchase_price = 600000
+current_value = 850000
+
+#update_asset(asset_class, identifier, current_value)
+#delete_asset(identifier)
+
+
+# iniitalise GUI
+window = tkinter.Tk()
+
+my_message = tkinter.Message(text="Hi. Welcome to my Example GUI Application!", width=1000)
+my_message = tkinter.Message(text="Hi. Welcome to my Example GUI Application!", width=1000)
+
+
+# ENTRY (TEXT INPUT) WITH LABEL
+
+my_label = tkinter.Label(text="Input something here:")
+entry_value = tkinter.StringVar()
+my_entry = tkinter.Entry(textvariable=entry_value)
+
+# RADIO BUTTONS
+
+my_radio_label = tkinter.Label(
+    text="Please selection one of the following options:")
+my_radio_value = tkinter.StringVar()
+my_radio_a = tkinter.Radiobutton(
+    text="Option A", value="A", variable=my_radio_value)
+my_radio_b = tkinter.Radiobutton(
+    text="Option B", value="B", variable=my_radio_value)
+my_radio_c = tkinter.Radiobutton(
+    text="Option C", value="C", variable=my_radio_value)
+
+# CHECKBUTTONS
+
+my_checkbox_group_label = tkinter.Label(
+    text="Please check one or more of the following boxes:")
+my_checkbox_a_val = tkinter.StringVar()
+my_checkbox_a = tkinter.Checkbutton(text="Box A", variable=my_checkbox_a_val)
+my_checkbox_b_val = tkinter.StringVar()
+my_checkbox_b = tkinter.Checkbutton(text="Box B", variable=my_checkbox_b_val)
+my_checkbox_c_val = tkinter.StringVar()
+my_checkbox_c = tkinter.Checkbutton(text="Box C", variable=my_checkbox_c_val)
+
+# LISTBOX (DROPDOWN SELECT)
+
+my_select_label = tkinter.Label(
+    text="Please select an item from the dropdown:")
+my_select = tkinter.Listbox()
+my_select.insert(1, "First Item")
+my_select.insert(2, "Second Item")
+my_select.insert(3, "Third Item")
+my_select.insert(4, "Fourth Item")
+my_select.insert(5, "Fifth Item")
+my_select.insert(6, "Sixth Item")
+
+# BUTTON
+
+
+def handle_button_click():
+    print("------------------------------")
+    print("NICE. YOU CLICKED THE BUTTON")
+    print("THE ENTRY'S INPUT VALUE IS:", my_entry.get())
+    print("THE SELECTED RADIO BUTTON'S VALUE IS:", my_radio_value.get())
+    print("THE CHECKBOX ON/OFF VALUES FOR A, B, C, RESPECTIVELY, ARE:",
+          [my_checkbox_a_val.get(), my_checkbox_b_val.get(), my_checkbox_c_val.get()])
+    print("THE SELECTED DROPDOWN ITEM IS:",
+          my_select.get(my_select.curselection()))
+
+
+my_button = tkinter.Button(text="Click Me", command=handle_button_click)
+
+#
+# BIND THE INDIVIDUAL COMPONENTS TO THE GUI WINDOW (PACK)
+# ... THEN LAUNCH THE GUI WINDOW (MAINLOOP)
+#
+
+my_message.pack()
+
+my_label.pack()
+my_entry.pack()
+
+my_radio_label.pack()
+my_radio_a.pack()
+my_radio_b.pack()
+my_radio_b.pack()
+
+my_checkbox_group_label.pack()
+my_checkbox_a.pack()
+my_checkbox_b.pack()
+my_checkbox_c.pack()
+
+my_select_label.pack()
+my_select.pack()
+
+my_button.pack()
+
+window.mainloop()
