56e8050d616ba4c468cc723e5ef36d7c2c393107
Justin Davda <justindavda@gmail.com>
2020-06-27 14:38:20 -0400

    delete item completed

--- app/update_files.py
+++ app/update_files.py
@@ -7,7 +7,35 @@ import os
 import pandas as pd 
 
 
-def update_ETP(asset_class,ticker,units,purchase_price):
+def delete_item(identifier, df, filepath):
+
+    #isThere = False
+    try:
+        check_ticker = str(identifier)
+
+    except ValueError:
+        error = "Invalid format - please check and try again"
+        print(error)
+        return error
+    
+    exists = df["Identifier"].str.contains(identifier).any()
+
+    if exists == True: 
+        df.drop(df[df.Identifier == identifier].index, inplace=True)
+        df.to_csv(filepath, index=False)
+        action = "deleted"   
+
+    elif exists != True:
+        action = "not found"
+
+    else:
+        action = "error!"
+
+    #print(identifier,action)
+    return action
+
+
+def update_etp(asset_class,identifier,units,purchase_price):
     #pull in data from csv
     isThere = False 
     etp_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "exchange_traded_products.csv")
@@ -15,7 +43,7 @@ def update_ETP(asset_class,ticker,units,purchase_price):
     
     try:
         check_asset_class = str(asset_class)
-        check_ticker = str(ticker)
+        check_identifier = str(identifier)
         check_units = int(units)
         check_purchase_price = float(purchase_price)
         
@@ -30,19 +58,19 @@ def update_ETP(asset_class,ticker,units,purchase_price):
         return error        
 
     for ind in df_etp.index:   
-        if df_etp["Ticker"][ind] == ticker:              
+        if df_etp["Identifier"][ind] == identifier:              
 
         #if ticker already exists, just update units, purchase price
         
-            df_etp.loc[df_etp["Ticker"] == ticker, ["Units"]] = units
-            df_etp.loc[df_etp["Ticker"] == ticker, ["Purchase Price"]] = round(purchase_price,2)
+            df_etp.loc[df_etp["Identifier"] == identifier, ["Units"]] = units
+            df_etp.loc[df_etp["Identifier"] == identifier, ["Purchase Price"]] = round(purchase_price,2)
             isThere = True
     
         #if ticker doesn't exist, add it to the end
 
         if isThere != True:
             df2 = pd.DataFrame({"Asset Class":[asset_class], 
-            "Ticker":[ticker], 
+            "Identifier":[identifier], 
             "Units":[units],"Purchase Price":[purchase_price]}) 
 
             dff = df_etp.append(df2, ignore_index = True)
@@ -58,15 +86,12 @@ def update_ETP(asset_class,ticker,units,purchase_price):
     #print(ticker,action)
     return action   
 
-def delete_ETP(ticker):
-    isThere = False
-    etp_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "exchange_traded_products.csv")
-    df_etp = pd.read_csv(etp_filepath)
+def delete_etp(identifier):
+    
+    filepath = os.path.join(os.path.dirname(__file__), "..", "data", "exchange_traded_products.csv")
+    df = pd.read_csv(filepath)
+    
+    delete_item(identifier, df, filepath)
+    return
+    
 
-    try:
-        check_ticker = str(ticker)
-        
-    except ValueError:
-        error = "Invalid format - please check and try again"
-        print(error)
-        return error
--- app/user_interface.py
+++ app/user_interface.py
@@ -1,12 +1,16 @@
 # user_interface.py
 # basic menus and decision-making around what user wants to do
 
-from update_files import update_ETP
+from app.update_files import update_etp
+from app.update_files import delete_etp
+
 import os
 
 asset_class = "Equity"
-ticker = "C"
+identifier = "GOOG"
 units = 10
 purchase_price = 100
 
-update_ETP(asset_class, ticker, units, purchase_price)
+#update_ETP(asset_class, identifier, units, purchase_price)
+
+delete_etp(identifier)
